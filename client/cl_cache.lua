0903bb79b218a5e724c5d8f023fdccb604edb873
iSentrie <xvender7@gmail.com>
2021-05-07 18:38:55 +0300

    Minified
    • removed `GetPlayerPed(-1)`, because `PlayerPedId()` is faster and does the same.
    • minified code.
    • script waits for player to load up, then starts his job.

--- client/cl_cache.lua
+++ client/cl_cache.lua
@@ -1,7 +1,13 @@
--- Created by Mascotte45
+local playerLoaded = false
 
+AddEventHandler("playerSpawned", function ()
+	if not playerLoaded then
+		playerLoaded = true
+	end
+end)
+
+-- Created by Mascotte45
 -- Default cache data 
---
 -- Don't remove anything from here unless you know what you are doing!
 AddEventHandler('mascotte-cache:getCacheData', function(cb)
 	cb(Cache)
@@ -11,7 +17,6 @@ function getCacheData()
 	return Cache
 end
 
-
 Cache = {}
 
 Cache.ClientPlayerId = PlayerId()
@@ -19,17 +24,12 @@ Cache.ClientPedId = PlayerPedId()
 Cache.PlayerFromServerId = GetPlayerFromServerId(Cache.ClientPlayerId)
 Cache.GetPlayerPed = GetPlayerPed(Cache.PlayerFromServerId)
 Cache.GetPlayerPedSource = GetPlayerPed(-1)
-
-
 Cache.ClientGetEntityCoords = GetEntityCoords(Cache.ClientPedId) -- Get the player coords
-
 Cache.IsPedOnFoot = IsPedOnFoot(Cache.PlayerPedId) -- True/False Is the player on foot or in a vehicle
-
 Cache.PlayersLastVehicle = GetPlayersLastVehicle() -- Last vehicle player was in
 Cache.IsPedSittingInAnyVehicle = IsPedSittingInAnyVehicle(Cache.ClientPedId) -- True/False Is the player sitting in a vehicle?
 Cache.GetVehiclePedIsCurrentlyIn = GetVehiclePedIsIn(Cache.ClientPedId, false) -- Vehicle Player is currently in 
 Cache.IsPedInAnyVehicle = IsPedInAnyVehicle(Cache.PlayerPedId, false) -- True/False is player in any kind of vehicle
-
 
 -- Static Data, so in other words, natives that will return the same data each time
 -- It's ran through this thread so we can set it to the data above in the cache table
@@ -37,19 +37,17 @@ Cache.IsPedInAnyVehicle = IsPedInAnyVehicle(Cache.PlayerPedId, false) -- True/Fa
 -- Don't remove anything from here unless you know what you are doing!
 
 Citizen.CreateThread(function()
+	while not playerLoaded do
+		Citizen.Wait(100)
+	end
 
-while true do
-
-    Cache.ClientPlayerId = PlayerId()
-    Cache.ClientPedId = PlayerPedId()
-    Cache.PlayerFromServerId = GetPlayerFromServerId(Cache.ClientPlayerId)
-    Cache.GetPlayerPed = GetPlayerPed(Cache.PlayerFromServerId)
-    Cache.GetPlayerPedSource = GetPlayerPed(-1)
-
-    Citizen.Wait(30000) -- Might still be a little too fast, I think this data doesn't/shouldn't change?
-
-end
-
+	while true do
+		Cache.ClientPlayerId = PlayerId()
+		Cache.ClientPedId = PlayerPedId()
+		Cache.PlayerFromServerId = GetPlayerFromServerId(Cache.ClientPlayerId)
+		Cache.GetPlayerPed = GetPlayerPed(Cache.PlayerFromServerId)
+		Citizen.Wait(30000) -- Might still be a little too fast, I think this data doesn't/shouldn't change?
+	end
 end)
 
 -- Dynamic Data. Data that changes regular and needs to be updated more often
@@ -58,27 +56,17 @@ end)
 -- Don't remove anything from here unless you know what you are doing!
 
 Citizen.CreateThread(function()
+	while not playerLoaded do
+		Citizen.Wait(100)
+	end
 
     while true do
-    
-    
         Cache.ClientGetEntityCoords = GetEntityCoords(Cache.ClientPedId)
-    
         Cache.GetVehiclePedIsCurrentlyIn = GetVehiclePedIsIn(Cache.ClientPedId, false)
-    
         Cache.IsPedInAnyVehicle = IsPedInAnyVehicle(Cache.ClientPlayerPedId, false)
-
         Cache.IsPedOnFoot = IsPedOnFoot(Cache.PlayerPedId)
-
         Cache.IsPedSittingInAnyVehicle = IsPedSittingInAnyVehicle(Cache.ClientPedId)
-
         Cache.PlayersLastVehicle = GetPlayersLastVehicle()
-
         Citizen.Wait(1000)
-    
     end
-
-
 end)
-
-

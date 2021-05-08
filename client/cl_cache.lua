acd6a93de360f1ba22bf7ee7122e4b2253f6b581
iSentrie <xvender7@gmail.com>
2021-05-08 12:25:36 +0300

    For smooth experience
    and f# no, we're not sacrificing anything.

--- client/cl_cache.lua
+++ client/cl_cache.lua
@@ -8,7 +8,6 @@ end)
 
 -- Created by Mascotte45
 -- Default cache data 
--- Don't remove anything from here unless you know what you are doing!
 AddEventHandler('mascotte-cache:getCacheData', function(cb)
 	cb(Cache)
 end)
@@ -30,44 +29,29 @@ Cache.IsPedSittingInAnyVehicle = IsPedSittingInAnyVehicle(Cache.ClientPedId) -- 
 Cache.GetVehiclePedIsCurrentlyIn = GetVehiclePedIsIn(Cache.ClientPedId, false) -- Vehicle Player is currently in 
 Cache.IsPedInAnyVehicle = IsPedInAnyVehicle(Cache.PlayerPedId, false) -- True/False is player in any kind of vehicle
 
--- Static Data, so in other words, natives that will return the same data each time
--- It's ran through this thread so we can set it to the data above in the cache table
---
--- Don't remove anything from here unless you know what you are doing!
 Citizen.CreateThread(function()
 	while true do
 		Cache.ClientPlayerId = PlayerId()
 		Cache.ClientPedId = PlayerPedId()
 		Cache.PlayerFromServerId = GetPlayerFromServerId(Cache.ClientPlayerId)
 		Cache.GetPlayerPed = GetPlayerPed(Cache.PlayerFromServerId)
-		Citizen.Wait(30000) -- Might still be a little too fast, I think this data doesn't/shouldn't change?
-	end
-end)
 
--- Dynamic Data. Data that changes regular and needs to be updated more often
--- It's ran through this thread so we can set it to the data above in the cache table
---
--- Don't remove anything from here unless you know what you are doing!
-Citizen.CreateThread(function()
-	while true do
 		Cache.ClientGetEntityCoords = GetEntityCoords(Cache.ClientPedId)
 		Cache.GetVehiclePedIsCurrentlyIn = GetVehiclePedIsIn(Cache.ClientPedId, false)
 		Cache.IsPedInAnyVehicle = IsPedInAnyVehicle(Cache.ClientPlayerPedId, false)
 		Cache.IsPedOnFoot = IsPedOnFoot(Cache.PlayerPedId)
 		Cache.IsPedSittingInAnyVehicle = IsPedSittingInAnyVehicle(Cache.ClientPedId)
 		Cache.PlayersLastVehicle = GetPlayersLastVehicle()
-		Citizen.Wait(1000)
+		Citizen.Wait(750)
 	end
 end)
 
 function loadAfterSpawn()
-	-- Piece of slow loop
 	Cache.ClientPlayerId = PlayerId()
 	Cache.ClientPedId = PlayerPedId()
 	Cache.PlayerFromServerId = GetPlayerFromServerId(Cache.ClientPlayerId)
 	Cache.GetPlayerPed = GetPlayerPed(Cache.PlayerFromServerId)
 
-	-- Piece of fast loop
 	Cache.ClientGetEntityCoords = GetEntityCoords(Cache.ClientPedId)
 	Cache.GetVehiclePedIsCurrentlyIn = GetVehiclePedIsIn(Cache.ClientPedId, false)
 	Cache.IsPedInAnyVehicle = IsPedInAnyVehicle(Cache.ClientPlayerPedId, false)
